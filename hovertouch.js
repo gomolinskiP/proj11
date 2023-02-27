417e713ce57f22516eb38369c4bb75388bbbc2d9
gomolinskiP <gomolinski.p@gmail.com>
2023-02-28 00:33:37 +0100

    fix

--- hovertouch.js
+++ hovertouch.js
@@ -1,8 +1,8 @@
 console.log("elo");
 
-var cellz = document.querySelectorAll("cellHover");
+let cellz = document.querySelectorAll("cellHover");
 
-cellz.forEach(e=>{
+cellz.forEach(function(e){
     e.addEventListener("touchstart", function(){
         e.classList.add("cellHover");
         console.log("hej");
