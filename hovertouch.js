e7f1acdad33284e7964987cc0c763da20471d286
gomolinskiP <gomolinski.p@gmail.com>
2023-02-28 11:57:59 +0100

    test

--- hovertouch.js
+++ hovertouch.js
@@ -4,14 +4,33 @@ let cellz = document.querySelectorAll(".cellH");
 
 console.log(cellz);
 
-cellz.forEach(function(e){
-    e.addEventListener("touchstart", function(){
+cellz.forEach(function (e) {
+    e.addEventListener("touchmove", function () {
         e.classList.add("cellHover");
         console.log("hej");
-        })
-        
-        e.addEventListener("touchend", function(){
+        e.addEventListener("touchmove", function () {
+            e.classList.add("cellHover");
+            console.log("hej");
+        });
+        setTimeout(function () {
+            e.classList.remove("cellHover");
+            console.log("timeout");
+        }, 2000);
+    });
+
+    e.addEventListener("touchend", function () {
+        e.classList.remove("cellHover");
+        console.log("narazie");
+        e.addEventListener("touchend", function () {
             e.classList.remove("cellHover");
             console.log("narazie");
-            })
-})
+        });
+    });
+
+    e.addEventListener("click", function () {
+        console.log("click");
+        e.addEventListener("click", function () {
+            console.log("click");
+        });
+    });
+});
